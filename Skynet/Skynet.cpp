f61b939c27dbb4d864d9d4b3c301782b7d561089
ageofshadows <willshui@gmail.com>
2017-05-21 13:03:18 +0800

    Add files via upload

---
+++ Skynet/Skynet.cpp
@@ -0,0 +1,38 @@
+// Skynet.cpp : Defines the entry point for the console application.
+//
+
+#include <stdafx.h>
+
+#include "IBCppClient.h"
+
+#include <boost/shared_ptr.hpp>
+
+
+int main(int argc, char** argv)
+{
+	const char* host = argc > 1 ? argv[1] : "";
+	unsigned int port = argc > 2 ? atoi(argv[2]) : 0;
+	if (port <= 0)
+		port = 7496;
+	int clientId = 0;
+
+	printf("Starting Skynet\n");
+
+	boost::shared_ptr<InteractiveBrokersCppClient> ibClient(new InteractiveBrokersCppClient());
+
+	//! [connect]
+	ibClient->connect(host, port, clientId);
+	//! [connect]
+
+	//! [ereader]
+	//Unlike the C# and Java clients, there is no need to explicitely create an EReader object nor a thread
+	while (ibClient->isConnected()) {
+		ibClient->processMessages();
+	}
+	//! [ereader]
+
+	printf("End of Skynet Engine\n");
+
+    return 0;
+}
+
